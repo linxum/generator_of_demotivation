7bb51a8ca57c2369130831651aadad2afba30b5c
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2023-01-27 01:37:10 +0300

    Update sts.py

--- sts.py
+++ sts.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import ImageFont
 
 def scale_textsize(txt, image):
 	fontsize = 1
@@ -6,10 +6,8 @@ def scale_textsize(txt, image):
 
 	font = ImageFont.truetype("Lobster-Regular.ttf", fontsize)
 	while int(font.getlength(txt)) < img_fraction * image.size[0]:
-		# iterate until the text size is just larger than the criteria
 		fontsize += 1
 		font = ImageFont.truetype("Lobster-Regular.ttf", fontsize)
 
-	# optionally de-increment to be sure it is less than criteria
 	fontsize -= 1
 	return fontsize
