abd8a836b17d0d28474370d797c79a458ab2d352
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2023-03-04 21:12:23 +0300

    Update sts.py

--- sts.py
+++ sts.py
@@ -1,13 +1,24 @@
 from PIL import ImageFont
 
-def scale_textsize(custom_text, img):
+
+def scale_textsize(custom_text, img, custom_font):
 	fontsize = 1
 	img_fraction = 0.50
 
-	font = ImageFont.truetype("Lobster-Regular.ttf", fontsize)
+	if custom_font == "impact":
+		font = ImageFont.truetype("impact.ttf", fontsize)
+	elif custom_font == "rodchenko":
+		font = ImageFont.truetype("Rodchenko.ttf", fontsize)
+	else:
+		font = ImageFont.truetype("Lobster.ttf", fontsize)
 	while int(font.getlength(custom_text)) < img_fraction * img.size[0]:
 		fontsize += 1
-		font = ImageFont.truetype("Lobster-Regular.ttf", fontsize)
+		if custom_font == "impact":
+			font = ImageFont.truetype("impact.ttf", fontsize)
+		elif custom_font == "rodchenko":
+			font = ImageFont.truetype("Rodchenko.ttf", fontsize)
+		else:
+			font = ImageFont.truetype("Lobster.ttf", fontsize)
 
 	fontsize -= 1
 	return fontsize
