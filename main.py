d84b13d36cf2799180da390a82fbfe4e108b0ed8
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2023-06-06 14:19:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from PIL import Image, ImageDraw, ImageFont
 import sts
 import time
 
-# bot token
-token = <YOUR TOKEN>
+token = '5904940309:AAGQ91eWPYObgvclRE-hPaQcj0VjhPAMBkI'
 custom_text = "вставить текст"
 custom_color = (255, 255, 255)
 custom_font = "lobster"
 custom_pos = False
+custom_size = 0
 
 # creating a bot object
 bot = telebot.TeleBot(token)
@@ -21,22 +21,18 @@ def com_start(message):
         message.chat.id, "Привет! Это генератор демотивации! Напиши текст, пришли картинку и получишь мем!")
 
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['help', 'помощь'])
 def com_help(message):
     bot.send_message(message.chat.id, "Изменение цвета текста (по умолчанию белый)")
-    time.sleep(1)
-    bot.send_message(message.chat.id, "/color [black] [red] [blue] [green]")
+    bot.send_message(message.chat.id, "/color [white] [black] [red] [blue] [green]")
     time.sleep(1)
     bot.send_message(message.chat.id, "Изменение цвета текста в системе RGB")
-    time.sleep(1)
     bot.send_message(message.chat.id, "/rgb <R> <G> <B>")
     time.sleep(1)
     bot.send_message(message.chat.id, "Изменение шрифта текста (по умолчанию Lobster)")
-    time.sleep(1)
-    bot.send_message(message.chat.id, "/font [impact] [rodchenko]")
+    bot.send_message(message.chat.id, "/font [lobster] [impact] [rodchenko]")
     time.sleep(1)
     bot.send_message(message.chat.id, "Изменение позиции текста (по умолчанию снизу)")
-    time.sleep(1)
     bot.send_message(message.chat.id, "/pos [внизу] [вверху]")
     time.sleep(1)
     bot.send_message(message.chat.id, "Дальше пишешь текст и отправляешь картику")
@@ -46,24 +42,26 @@ def extract_arg(arg):
     return arg.split()[1:]
 
 
-@bot.message_handler(commands=['color'])
+@bot.message_handler(commands=['color', 'цвет'])
 def setup_color(message):
     global custom_color
     set_color = extract_arg(message.text)
     if set_color:
-        if set_color[0] == "black" or set_color[0] == "Black":
+        if set_color[0] == "black" or set_color[0] == "Black" or set_color[0] == "черный" or set_color[0] == "Черный"  \
+                or set_color[0] == "чёрный" or set_color[0] == "Чёрный":
             custom_color = (0, 0, 0)
             bot.reply_to(message, "Принял черного")
-        elif set_color[0] == "white" or set_color[0] == "white":
+        elif set_color[0] == "white" or set_color[0] == "White" or set_color[0] == "белый" or set_color[0] == "Белый":
             custom_color = (255, 255, 255)
             bot.reply_to(message, "Принял белого")
-        elif set_color[0] == "red" or set_color[0] == "Red":
+        elif set_color[0] == "red" or set_color[0] == "Red" or set_color[0] == "красный" or set_color[0] == "Красный":
             custom_color = (255, 0, 0)
             bot.reply_to(message, "Принял красного")
-        elif set_color[0] == "blue" or set_color[0] == "Blue":
+        elif set_color[0] == "blue" or set_color[0] == "Blue" or set_color[0] == "Синий" or set_color[0] == "синий":
             custom_color = (0, 0, 255)
             bot.reply_to(message, "Принял синего")
-        elif set_color[0] == "green" or set_color[0] == "Green":
+        elif set_color[0] == "green" or set_color[0] == "Green" or set_color[0] == "зеленый" or set_color[0] == "Зеленый" \
+                or set_color[0] == "Зелёный" or set_color[0] == "зелёный":
             custom_color = (0, 255, 0)
             bot.reply_to(message, "Принял зеленого")
         else:
@@ -71,7 +69,8 @@ def setup_color(message):
     else:
         bot.reply_to(message, "Не понял, повтори")
 
-@bot.message_handler(commands=['rgb'])
+
+@bot.message_handler(commands=['rgb', 'ргб', 'кзс'])
 def setup_rgb(message):
     global custom_color
     set_color = extract_arg(message.text)
@@ -82,7 +81,7 @@ def setup_rgb(message):
         bot.reply_to(message, "Не понял, повтори")
 
 
-@bot.message_handler(commands=['font'])
+@bot.message_handler(commands=['font', 'шрифт'])
 def setup_font(message):
     global custom_font
     set_font = extract_arg(message.text)
@@ -102,7 +101,7 @@ def setup_font(message):
         bot.reply_to(message, "Не понял, повтори")
 
 
-@bot.message_handler(commands=['pos'])
+@bot.message_handler(commands=['pos', 'поз'])
 def setup_pos(message):
     global custom_pos
     set_pos = extract_arg(message.text)
@@ -119,12 +118,26 @@ def setup_pos(message):
         bot.reply_to(message, "Не понял, повтори")
 
 
+@bot.message_handler(commands=['size', 'размер'])
+def setup_size(message):
+    global custom_size
+    set_size = extract_arg(message.text)
+    if set_size:
+        if set_size[0] == "0" or set_size[0] == "0":
+            custom_size = 0
+            bot.reply_to(message, "Размер текста будет выбран автоматически")
+        elif int(set_size[0]) > 0:
+            custom_size = int(set_size[0])
+            bot.reply_to(message, "Размер текста: " + set_size[0])
+        else:
+            bot.reply_to(message, "Не понял, повтори")
+    else:
+        bot.reply_to(message, "Не понял, повтори")
+
+
 @bot.message_handler(content_types=['photo'])
 def create_photo(message):
-    global custom_text, custom_color, custom_font, custom_pos
-    # print(custom_text)
-    # print(custom_color)
-    # print(custom_font)
+    global custom_text, custom_color, custom_font, custom_pos, custom_size
     fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(
@@ -138,12 +151,20 @@ def create_photo(message):
     img = Image.open("photo.jpg")
     draw = ImageDraw.Draw(img)
 
-    if custom_font == "impact":
-        font = ImageFont.truetype("impact.ttf", size=sts.scale_textsize(custom_text, img, custom_font))
-    elif custom_font == "rodchenko":
-        font = ImageFont.truetype("Rodchenko.ttf", size=sts.scale_textsize(custom_text, img, custom_font))
+    if custom_size == 0:
+        if custom_font == "impact":
+            font = ImageFont.truetype("impact.ttf", size=sts.scale_textsize(custom_text, img, custom_font))
+        elif custom_font == "rodchenko":
+            font = ImageFont.truetype("Rodchenko.ttf", size=sts.scale_textsize(custom_text, img, custom_font))
+        else:
+            font = ImageFont.truetype("Lobster.ttf", size=sts.scale_textsize(custom_text, img, custom_font))
     else:
-        font = ImageFont.truetype("Lobster.ttf", size=sts.scale_textsize(custom_text, img, custom_font))
+        if custom_font == "impact":
+            font = ImageFont.truetype("impact.ttf", size=custom_size)
+        elif custom_font == "rodchenko":
+            font = ImageFont.truetype("Rodchenko.ttf", size=custom_size)
+        else:
+            font = ImageFont.truetype("Lobster.ttf", size=custom_size)
 
     W, H = img.size
     w, h = font.getbbox(custom_text)[2] - font.getbbox(custom_text)[0], font.getbbox(custom_text)[3]
@@ -158,8 +179,9 @@ def create_photo(message):
     bot.send_photo(message.chat.id, open('photo_edited.jpg', 'rb'))
     custom_text = "вставить текст"
     custom_color = (255, 255, 255)
-    custom_pos = False
     custom_font = "lobster"
+    custom_pos = False
+    custom_size = 0
 
 
 @bot.message_handler(content_types=['text'])
